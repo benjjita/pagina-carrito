95d6eec832108810be34e668f3452385e8b22328
benjjita <117098949+benjjita@users.noreply.github.com>
2024-12-12 21:19:18 -0300

    Delete java.js

--- java.js
+++
@@ -1,8 +0,0 @@
-const alumnos = []
-
-function ingresar() {
-	let nombre = document.getElementById('nombre').value;
-	let apellido = document.getElementById('apellido').value;
-	let edad = document.getElementById('edad').value;
-	alert("Usuario: " + nombre + apellido + edad)
-}
